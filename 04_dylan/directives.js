c9169e27df254b5d8231fc77d5dcf1d81a95995b
Greg Sandell <greg@quietfoundation.com>
2013-11-01 11:24:18 -0500

    Removed superfluous directives file

--- 04_dylan/directives.js
+++
@@ -1,9 +0,0 @@
-angular.module('myapp').directive('transcludeMe', function($compile) {
-    return {
-        restrict: 'E',
-        template: "<div>The answer, my friend,<div ng-transclude></div>the answer is blowin' in the wind</div>",
-        replace: true,
-        transclude: true
-    };
-});
-
